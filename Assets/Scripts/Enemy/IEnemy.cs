10c11fb9e3e839160f6303bf8c9b9a18ae7d2f15
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-11-16 17:26:12 +0300

    making enemy logic

---
+++ Assets/Scripts/Enemy/IEnemy.cs
@@ -0,0 +1,7 @@
+public interface IEnemy
+{
+    public float EnemyCurrentHealth { get; set; }
+    public float EnemyMaxHealth { get; set; }
+    public float EnemyDamage { get; set; }
+    public float EnemySpeed { get; set; }
+}
