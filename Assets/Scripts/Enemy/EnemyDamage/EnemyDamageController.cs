21ef6c93ceea4adda05976f6c3eb8a5c20ad969a
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-11-21 22:59:59 +0300

    fixing bugs with trigger

--- Assets/Scripts/Enemy/EnemyDamage/EnemyDamageController.cs
+++ Assets/Scripts/Enemy/EnemyDamage/EnemyDamageController.cs
@@ -9,10 +9,13 @@ public class EnemyDamageController : MonoBehaviour
     private IEnemyDamage _enemyDamageModel;
 
     [SerializeField] private EnemyDamageConfig _enemyDamageConfig;
+    [SerializeField] private GameObject _playerGameObject;
+
     private PlayerHealthController _playerHealthController;
 
-    public float _attackCooldown = 3f;
-    private bool _canAttack = true; 
+    [SerializeField] private float _attackCooldown = 3f;
+
+    private bool _canAttack;
 
     [Inject]
     public void Construct(PlayerHealthController playerHealthController)
@@ -30,29 +33,29 @@ public class EnemyDamageController : MonoBehaviour
         {
             Debug.Log("No enemy damage model found");
         }
+        _canAttack = true;
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.GetComponent<PlayerHealthView>())
-        {
-            if (_canAttack)
-            {
-                StartCoroutine(AttackWithCooldown());
-            }
-        }
-    }
-
+    
     IEnumerator AttackWithCooldown()
     {
+        AttackPlayer();
         _canAttack = false;
         yield return new WaitForSeconds(_attackCooldown);
-        AttackPlayer();
         _canAttack = true;
     }
 
     void AttackPlayer()
     {
-        _playerHealthController.PlayerTakeDamage(_enemyDamageModel.EnemyDamageAmount);
+        if (_playerGameObject != null)
+        {
+            _playerHealthController.PlayerTakeDamage(_enemyDamageModel.EnemyDamageAmount);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (_canAttack && collision.gameObject == _playerGameObject)
+        {
+            StartCoroutine(AttackWithCooldown());
+        }
     }
 }
