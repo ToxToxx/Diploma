4563e3b70c75c5e7bf82023cf6e130b248eb36a1
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-11-21 21:28:09 +0300

    making enemy damage controller

--- Assets/Scripts/Player/PlayerHealth/PlayerHealthController.cs
+++ Assets/Scripts/Player/PlayerHealth/PlayerHealthController.cs
@@ -7,28 +7,32 @@ public class PlayerHealthController : ITickable
     public event Action<float> OnHealthDecreased;
     public event EventHandler OnPlayerDeath;
     private PlayerHealthModel _healthModel;
+    private bool _isSecondChance;
 
     [Inject]
     public void Construct(PlayerHealthModel healthModel)
     {
         _healthModel = healthModel;
+        _isSecondChance = true;
     }
 
 
     public void Tick()
     {
-        PlayerTakeDamage();
+
     }
 
-    public void PlayerTakeDamage()
+    public void PlayerTakeDamage(float damageAmount)
     {
         if (_healthModel.Health > 0)
         {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                _healthModel.Health -= 10;
-                OnHealthDecreased(_healthModel.Health);
-            }
+            _healthModel.Health -= damageAmount;
+            OnHealthDecreased(_healthModel.Health);
+        }
+        else if (_isSecondChance)
+        {
+            _healthModel.Health = 5;
+            _isSecondChance = false;
         }
         else
         {
