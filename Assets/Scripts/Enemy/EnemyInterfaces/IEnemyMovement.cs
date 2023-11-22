e187cc37bd5334a401ed711329771f93980d131f
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-11-22 16:22:35 +0300

    making enemy movement logic, made interface

--- Assets/Scripts/Enemy/EnemyInterfaces/IEnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyInterfaces/IEnemyMovement.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IEnemyMovement : MonoBehaviour
+public interface IEnemyMovement
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float Speed { get; set; }
 }
