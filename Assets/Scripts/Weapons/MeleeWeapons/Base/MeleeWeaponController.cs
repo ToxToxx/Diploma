32d1b93f37c93cb7d030f02eb3e620e93da76506
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-11-23 22:35:13 +0300

    refactore parameteres

--- Assets/Scripts/Weapons/MeleeWeapons/Base/MeleeWeaponController.cs
+++ Assets/Scripts/Weapons/MeleeWeapons/Base/MeleeWeaponController.cs
@@ -1,10 +1,10 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Zenject;
 
-public class MeleeWeaponController : MonoBehaviour
+public class MeleeWeaponController : MonoBehaviour, IDisposable
 {
     [SerializeField] private MeleeWeaponConfig _meleeWeaponConfig;
     [SerializeField] private LayerMask _enemyMask;
@@ -13,6 +13,8 @@ public class MeleeWeaponController : MonoBehaviour
     private MeleeWeaponTypeController _meleeWeaponTypeController;
 
     private PlayerInputSystem _playerInputSystem;
+    private bool canAttack = true;
+    private float AttackSpeedCoef = 1.0f;
 
     [Inject]
     public void Construct(PlayerInputSystem playerInputSystem)
@@ -21,19 +23,32 @@ public class MeleeWeaponController : MonoBehaviour
         _playerInputSystem.OnAttackPlayerInputPerformed += OnMeleeWeaponAttackPerformed;
     }
 
-    private void OnMeleeWeaponAttackPerformed(InputAction.CallbackContext obj)
-    {
-        PerformAttack();
-    }
-
     private void Awake()
     {
         _meleeWeaponTypeController = new MeleeWeaponTypeController();
         _meleeWeaponModel = _meleeWeaponTypeController.InitializeMeleeWeaponType(_meleeWeaponConfig);
     }
+
+    private void OnMeleeWeaponAttackPerformed(InputAction.CallbackContext obj)
+    {
+        if (canAttack)
+        {
+            StartCoroutine(AttackCooldown());
+            PerformAttack();
+        }
+    }
+
+    private IEnumerator AttackCooldown()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(AttackSpeedCoef / _meleeWeaponModel.AttackSpeed);
+        canAttack = true;
+    }
+
     private void PerformAttack()
     {
         Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(transform.position, _meleeWeaponModel.AttackDistance, _enemyMask);
+
         foreach (var enemy in enemiesToDamage)
         {
             if (enemy.GetComponent<EnemyHealthController>())
@@ -42,5 +57,10 @@ public class MeleeWeaponController : MonoBehaviour
                 Debug.Log(enemy.GetComponent<EnemyHealthController>().GetHealth());
             }
         }
+    }
+
+    public void Dispose()
+    {
+        _playerInputSystem.OnAttackPlayerInputPerformed -= OnMeleeWeaponAttackPerformed;
     }
 }
