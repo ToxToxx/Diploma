95a0d3e82e437bf5ceede27c6e37f25feaeb9ff2
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-12-07 16:36:04 +0300

    made players inventory window controller
    It's controlling window for open and closing

--- Assets/Scripts/UI/PlayersInventoryWindowController.cs
+++ Assets/Scripts/UI/PlayersInventoryWindowController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class PlayersInventoryWindowController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputSystem _playerInputSystem;
+    private bool _isInventoryOpen;
+    [SerializeField] private GameObject _playerInventoryWindow;
+
+    [Inject]
+    public void Construct(PlayerInputSystem playerInputSystem)
     {
-        
+        _playerInputSystem = playerInputSystem;
+        _playerInputSystem.OnInventoryWindowOpenPerformed += PlayerInputSystemOnInventoryOpenPerformed;
+        Hide();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlayerInputSystemOnInventoryOpenPerformed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        
+        if(_isInventoryOpen)
+        {
+            Hide();
+        }
+        else
+        {
+            Show();
+        }
+    }
+
+
+    public void Show()
+    {
+        _playerInventoryWindow.SetActive(true);
+        _isInventoryOpen = true;
+    }
+
+    public void Hide()
+    {
+        _playerInventoryWindow.SetActive(false);
+        _isInventoryOpen = false;
     }
 }
