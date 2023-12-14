f99694e9833ef4cb7b35e922f3543b5d5cd88bd1
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-12-14 15:00:12 +0300

    made ui for ammo, current ammo and how much in inventory

--- Assets/Scripts/Weapons/RangedWeapons/Base/RangedWeaponInventoryFirstView.cs
+++ Assets/Scripts/Weapons/RangedWeapons/Base/RangedWeaponInventoryFirstView.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 public class RangedWeaponInventoryFirstView : MonoBehaviour
 {
-    [SerializeField] private RangedWeaponController _rangedWeaponController;
+    private RangedWeaponController _rangedWeaponController;
     [SerializeField] private Image _rangedWeaponImage;
+    [SerializeField] private TextMeshProUGUI _rangedWeaponAmmoText;
+
+    private PlayerInventoryController _playerInventoryController;
+    private int _ammoCount;
+
+    [Inject]
+    public void Construct(PlayerInventoryController playerInventoryController, RangedWeaponController rangedWeaponController)
+    {
+        _playerInventoryController = playerInventoryController;
+        _rangedWeaponController = rangedWeaponController;
+    }
 
     private void Update()
     {
@@ -14,6 +25,21 @@ public class RangedWeaponInventoryFirstView : MonoBehaviour
         {
             _rangedWeaponImage.sprite = _rangedWeaponController.GetRangedWeaponConfig().RangedWeaponIcon;
         }
+        _rangedWeaponAmmoText.text = _rangedWeaponController.GetRangedWeaponModel().CurrentAmmo.ToString() + "/" + GetAmmoCount();
     }
 
+    private int GetAmmoCount()
+    {
+        _ammoCount = 0; 
+
+        foreach (GameObject playerInventoryItem in _playerInventoryController.GetPlayerInventory())
+        {
+            if (_rangedWeaponController.GetRangedWeaponConfig().RangedWeaponAmmoType == playerInventoryItem.GetComponent<IInventoryItem>().Type)
+            {
+                _ammoCount += playerInventoryItem.GetComponent<IInventoryItem>().ItemCount;
+            }
+        }
+
+        return _ammoCount;
+    }
 }
